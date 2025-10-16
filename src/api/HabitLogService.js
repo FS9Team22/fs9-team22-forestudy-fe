f17f3ce8d512a83fd1125f43d248399271667ee6
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-16 16:04:18 +0900

    feat: 상세 페이지를 위한 API 호출 구현

---
+++ src/api/HabitLogService.js
@@ -0,0 +1,13 @@
+const API_URL = import.meta.env.VITE_API_URL + '/habit';
+
+export const getHabitLogListByHabitId = async function ({ habitId }) {
+  const url = `${API_URL}/${habitId}/log`;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(
+      `리퀘스트 에러: ${res.status}, 에러 메시지: ${res.statusText}`,
+    );
+  }
+  const data = await res.json();
+  return data;
+};
---
+++ src/api/StudyService.js
@@ -0,0 +1,13 @@
+const API_URL = import.meta.env.VITE_API_URL + '/study';
+
+export const getStudyById = async function ({ id }) {
+  const url = `${API_URL}/${id}`;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(
+      `리퀘스트 에러: ${res.status}, 에러 메시지: ${res.statusText}`,
+    );
+  }
+  const data = await res.json();
+  return data;
+};
