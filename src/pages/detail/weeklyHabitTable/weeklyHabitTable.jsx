e60cb8a9ef44a58e4d5a49fc6d4321d298fee174
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-17 14:32:08 +0900

    feat: 습관 기록표 전체 레이아웃

---
+++ src/pages/detail/weeklyHabitTable/weeklyHabitTable.jsx
@@ -0,0 +1,53 @@
+import './weeklyHabitTable.css';
+import { WeeklyHabitRow } from '../weeklyHabitRow';
+
+export function WeeklyHabitTable() {
+  // 임시 데이터 배열
+  const habits = [
+    {
+      id: 1,
+      name: '아침 30분 조깅',
+      data: [true, true, false, true, true, false, true],
+    },
+    {
+      id: 2,
+      name: '일어나서 물 한잔',
+      data: [true, true, true, true, true, true, true],
+    },
+    {
+      id: 3,
+      name: '책 20페이지 읽기',
+      data: [true, false, true, false, true, true, false],
+    },
+    {
+      id: 4,
+      name: '하루 1만보 걷기',
+      data: [false, false, true, true, true, true, true],
+    },
+  ];
+
+  return (
+    <div className="section-weeklyhabit-wrap">
+      <h2 className="middle-header">습관 기록표</h2>
+      <ul className="weekname">
+        <li>월</li>
+        <li>화</li>
+        <li>수</li>
+        <li>목</li>
+        <li>금</li>
+        <li>토</li>
+        <li>일</li>
+      </ul>
+      <div className="habit-list-box">
+        {habits.map((habit, rowIndex) => (
+          <WeeklyHabitRow
+            key={habit.id}
+            index={rowIndex}
+            habitName={habit.name}
+            weeklyData={habit.data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
