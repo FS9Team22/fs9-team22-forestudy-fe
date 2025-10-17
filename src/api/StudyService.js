42323e53f9a632227807d2b9bc6aeb66c7a044d0
Gephigirl <maestra_jazmin@naver.com>
2025-10-17 17:04:57 +0900

    Merge pull request #7 from rklpoi5678/dev
    랜딩페이지, 만들기페이지

--- src/api/StudyService.js
+++ src/api/StudyService.js
@@ -1,17 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_URL + '/study';
 
-export const getStudyById = async function ({ id }) {
-  const url = `${BASE_URL}/${id}`;
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(
-      `리퀘스트 에러: ${res.status}, 에러 메시지: ${res.statusText}`,
-    );
-  }
-  const data = await res.json();
-  return data;
-};
-
 export async function createStudy(
   nickname,
   title,
@@ -60,3 +48,15 @@ export async function getStudyList(orderBy, keyword, page, limit) {
     throw err;
   }
 }
+
+export const getStudyById = async function ({ id }) {
+  const url = `${BASE_URL}/${id}`;
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(
+      `리퀘스트 에러: ${res.status}, 에러 메시지: ${res.statusText}`,
+    );
+  }
+  const data = await res.json();
+  return data;
+};
