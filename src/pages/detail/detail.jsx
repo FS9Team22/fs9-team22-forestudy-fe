61c6a659a0374767b4e374df94a694963e45de34
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-20 15:37:09 +0900

    feat: 상세페이지에서 스터디 정보를 보여주는 부분 분리

--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -5,6 +5,7 @@ import './detail.css';
 import { WeeklyHabitTable } from './weeklyHabitTable';
 import { getStudyById } from '../../api/StudyService';
 import { getHabitListByStudyId } from '../../api/HabitService';
+import { StudyInfo } from '../../components/StudyInfo';
 
 export default function DetailPage() {
   const { studyId } = useParams();
@@ -53,28 +54,8 @@ export default function DetailPage() {
               <li>삭제하기</li>
             </ul>
           </div>
-          <div className="section-title-wrap">
-            <h1 className="title-box large-header">
-              <span className="study-nickname">{study.nickname}</span>
-              <span>의 </span>
-              <span className="study-title">{study.title}</span>
-            </h1>
-            <ul className="link-btn-list">
-              <li>습관버튼컴포넌트</li>
-              <li>집중버튼컴포넌트</li>
-            </ul>
-          </div>
-          <div className="section-mid-wrap">
-            <div className="description-box">
-              <h3 className="small-header">소개</h3>
-              <p className="study-description">{study.description}</p>
-            </div>
-            <div className="point-box">
-              <h3 className="small-header">현재까지 획득한 포인트</h3>
-              <div>포인트알약컴포넌트</div>
-            </div>
-            <WeeklyHabitTable habits={habits} />
-          </div>
+          <StudyInfo study={study} />
+          <WeeklyHabitTable habits={habits} />
         </section>
       </main>
     </>
