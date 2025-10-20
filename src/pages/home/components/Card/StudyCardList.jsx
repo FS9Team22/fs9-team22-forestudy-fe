ee673e59b4b8e83bc670501481f359512cb7fa9a
rklpoi5678 <rklpoi5678@naver.com>
2025-10-20 13:48:03 +0900

    feat:  useLocalStorage훅으로 사용자가 클릭시 최근 조회한 스터디로

--- src/pages/home/components/Card/StudyCardList.jsx
+++ src/pages/home/components/Card/StudyCardList.jsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router';
+import { useLocalStorage } from '../../../../hooks/useLocalStorage';
 import { Card } from './Card';
 import styles from './StudyCardList.module.css';
 
+const ONE_HOUR = 60 * 60 * 1000;
+const MAX_STUDIES_LENGTH = 3;
 export function StudyCardList({ cards }) {
+  const [recentStudies, setRecentStudies] = useLocalStorage(
+    'recentStudies',
+    [],
+    ONE_HOUR,
+  );
+  const handleOnClick = (study) => {
+    const updatedStudies = recentStudies.filter((item) => item.id !== study.id);
+    updatedStudies.unshift(study);
+    if (updatedStudies.length > MAX_STUDIES_LENGTH) {
+      updatedStudies.pop();
+    }
+    setRecentStudies(updatedStudies);
+  };
+
   return (
     <div className={styles.cardListSection}>
       <div className={styles.cardListContainer}>
         {cards.map((card) => (
-          <Link to={`/study/${card.id}`}>
+          <Link to={`/study/${card.id}`} onClick={() => handleOnClick(card)}>
             <Card
               key={card.id}
               title={card.title}
