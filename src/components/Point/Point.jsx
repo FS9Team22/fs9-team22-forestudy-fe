968f3fcd2c57ea4e685c40daf2b35e5fa576fc19
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-24 11:16:41 +0900

    refactor: 아이콘 리팩토링

--- src/components/Point/Point.jsx
+++ src/components/Point/Point.jsx
@@ -1,15 +1,12 @@
 import styles from './Point.module.css';
+import pointIcon from '../../assets/icons/ic_point.svg';
 
 const Point = ({ points }) => {
   return (
     <div className={styles.pointsSection}>
       <p className={styles.smallHeader}>현재까지 획득한 포인트</p>
       <span className={styles.point}>
-        <img
-          src="/images/ic_point.png"
-          alt="point"
-          className={styles.pointIcon}
-        />
+        <img src={pointIcon} alt="point" className={styles.pointIcon} />
         {points}P 획득
       </span>
     </div>
