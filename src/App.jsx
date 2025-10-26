a9802dbf78de6f6fcb26ae1069856b585f27f69e
rklpoi5678 <rklpoi5678@naver.com>
2025-10-26 17:11:34 +0900

    feat: not-found  페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,28 @@ import CreatePage from './pages/create/create';
 import DetailPage from './pages/detail/detail';
 import HabitPage from './pages/habit/DailyHabit';
 import TimerPage from './pages/timer/timer';
+import NotFoundRoute from './pages/not-found';
 import { Nav } from './components/Nav/Nav';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        {/* 홈 */}
-        <Route path="/" element={<Home />} />
-        {/* 만들기 */}
-        <Route path="/create" element={<CreatePage />} />
-        {/* 스터디 상세 */}
-        <Route path="/study/:studyId" element={<DetailPage />} />
-        {/* <Route path="/study/:studyId/update" element={<FormPage />} /> */}
-        <Route path="/study/:studyId/habit" element={<HabitPage />} />
-        <Route path="/study/:studyId/timer" element={<TimerPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          {/* 홈 */}
+          <Route path="/" element={<Home />} />
+          {/* 만들기 */}
+          <Route path="/create" element={<CreatePage />} />
+          {/* 스터디 상세 */}
+          <Route path="/study/:studyId" element={<DetailPage />} />
+          {/* <Route path="/study/:studyId/update" element={<FormPage />} /> */}
+          <Route path="/study/:studyId/habit" element={<HabitPage />} />
+          <Route path="/study/:studyId/timer" element={<TimerPage />} />
+          <Route path="*" element={<NotFoundRoute />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
