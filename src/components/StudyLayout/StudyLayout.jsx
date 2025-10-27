356f649cc5677c70cb8326205ee23972b36abadc
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-27 17:13:17 +0900

    feat: 스터디 삼대 페이지 골조 레이아웃 컴포넌트화

--- src/components/StudyLayout/StudyLayout.jsx
+++ src/components/StudyLayout/StudyLayout.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { WeeklyHabitTable } from './weeklyHabitTable';
+import { useMatch, useParams } from 'react-router';
 import { getStudyById } from '@/api/StudyService';
 import { getHabitListByStudyId } from '@/api/HabitService';
-import './detail.css';
-import StudyLayout from '@/components/StudyLayout/StudyLayout';
+import { StudyInfo } from '@/components/StudyInfo';
+import DetailLayoutTop from './DetailLayoutTop';
+import './Layout.css';
 
-export default function DetailPage() {
+const WEB_URL = import.meta.env.VITE_WEB_URL;
+
+export default function StudyLayout({ children }) {
   const { studyId } = useParams();
+  const isDetail = useMatch('/study/:studyId');
   const [study, setStudy] = useState(null);
-  const [habits, setHabits] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -17,12 +19,11 @@ export default function DetailPage() {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const [studyResponse, habitResponse] = await Promise.all([
+        const [studyResponse] = await Promise.all([
           getStudyById({ id: studyId }),
           getHabitListByStudyId({ studyId }),
         ]);
         setStudy(studyResponse.data);
-        setHabits(habitResponse.data);
       } catch (err) {
         setError(err.message);
         console.error('데이터를 불러오는 데 실패했습니다.', err);
@@ -39,8 +40,18 @@ export default function DetailPage() {
   if (!study) return <div>스터디 정보를 찾을 수 없습니다.</div>;
 
   return (
-    <StudyLayout>
-      <WeeklyHabitTable habits={habits} />
-    </StudyLayout>
+    <>
+      <title>{'공부의 숲 | ' + study.nickname + '의 ' + study.title}</title>
+      <meta property="og:title" content={'공부의 숲 | ' + study.title} />
+      <meta property="og:url" content={WEB_URL + '/study/' + study.id} />
+      <meta property="og:description" content={study.description} />
+      <main className="layout-main">
+        <section className="layout-section">
+          {isDetail && <DetailLayoutTop studyId={study.id} />}
+          <StudyInfo study={study} />
+          {children}
+        </section>
+      </main>
+    </>
   );
 }
